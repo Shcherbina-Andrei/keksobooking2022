f23285eb4e10abecfb7147b5952ad66dd3cb41cf
andrei110295@yandex.ru <andrei110295@yandex.ru>
2022-09-19 13:04:57 +0300

    Добавил функции деактивации/активации форм на странице

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import {createNotices} from './util.js';
 import {displayNotices} from './similiar-notices.js';
+import {inActivePage} from './form-notice.js';
+import {activePage} from './form-notice.js';
 
+inActivePage();
+activePage();
 displayNotices(createNotices(1));
